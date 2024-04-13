8de553f4067f1922a59484e80432f2e1a8d14dce
江建清 <1810422805@qq.com>
2024-04-13 20:27:53 +0800

    vue引入路由

---
+++ my-home-client/src/router/index.js
@@ -0,0 +1,20 @@
+// 导入 Vue Router  
+import { createRouter, createWebHistory } from 'vue-router';
+
+// 导入需要路由的组件  
+import HelloWorld from './../components/HelloWorld.vue'
+
+// 定义路由  
+const routes = [{
+  path: '/hello',
+  name: 'HelloWorld',
+  component: HelloWorld
+}];
+
+// 创建 router 实例  
+const router = createRouter({
+  history: createWebHistory(),
+  routes // (缩写) 相当于 routes: routes  
+});
+
+export default router;

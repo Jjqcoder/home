26c24ef78041455f65c76974cec6c8cc5046f266
江建清 <1810422805@qq.com>
2024-06-08 09:50:24 +0800

    fix:解决移动端适配问题

--- my-home-client/src/components/blog/index.vue
+++ my-home-client/src/components/blog/index.vue
@@ -8,7 +8,7 @@
     <div class="demonstration"></div>
     <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize"
       :page-sizes="[5, 8, 10]" :small="small" :disabled="disabled" :background="background"
-      :pager-count="4" layout="prev, sizes, pager, next" :total="totalDataCount"
+      :pager-count="4" layout="prev, pager, next" :total="totalDataCount"
       @size-change="handleSizeChange" @current-change="handleCurrentChange" />
     <!--下方是完全的: layout="total, sizes, prev, pager, next, jumper" :total="totalDataCount" -->
   </div>

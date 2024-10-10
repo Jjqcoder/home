046fb548c41eb4654c93cdcf1d94b538ac6337d2
江建清 <1810422805@qq.com>
2024-10-10 21:10:32 +0800

    feat:解决跨域问题

--- manage-server-springboot/src/main/java/com/manageserverspringboot/config/CorsConfig.java
+++ manage-server-springboot/src/main/java/com/manageserverspringboot/config/CorsConfig.java
@@ -1,2 +1,23 @@
-package com.manageserverspringboot.config;public class CorsConfig {
+package com.manageserverspringboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+* @author Jiangjianqing
+* @date 2024/10/10 20:56
+* @description 允许跨域
+*/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 添加映射路径
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 允许任何来源访问
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
+                .allowedHeaders("*") // 允许的请求头
+                .maxAge(168000); // 预检请求的缓存时间（以秒为单位）
+    }
 }

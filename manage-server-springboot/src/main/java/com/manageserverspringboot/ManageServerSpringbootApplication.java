bb62361a5f0ef419245acd470fa305b365406f27
江建清 <1810422805@qq.com>
2024-10-23 21:31:17 +0800

    feat:引入sa-token

--- manage-server-springboot/src/main/java/com/manageserverspringboot/ManageServerSpringbootApplication.java
+++ manage-server-springboot/src/main/java/com/manageserverspringboot/ManageServerSpringbootApplication.java
@@ -1,14 +1,17 @@
 package com.manageserverspringboot;
 
+import cn.dev33.satoken.SaManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+@Slf4j
 @SpringBootApplication
 public class ManageServerSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManageServerSpringbootApplication.class, args);
+        log.info("启动成功，Sa-Token 配置如下：{}", SaManager.getConfig());
     }
 
 }
--- manage-server-springboot/src/main/java/com/manageserverspringboot/controller/TestController.java
+++ manage-server-springboot/src/main/java/com/manageserverspringboot/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.manageserverspringboot.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 */
 
 @RestController
-@Api(tags = "测试路由")
+@Api(tags = "☕测试路由")
 @RequestMapping("/test")
 public class TestController {
 
@@ -23,4 +24,20 @@ public class TestController {
     public String hello(@RequestParam(value = "msg", required = false, defaultValue = "Default Message1") String msg1, @RequestParam(value = "msg2", required = false, defaultValue = "Default Message2")String msg2) {
         return msg1 + "" + msg2;
     }
+    @ApiOperation(value = "sa-token登录简单测试", notes = "主要测试的内容为:用户登录之后分发token")
+    @GetMapping("/login")
+    public String saTokenTest(String username, String password) {
+        if ("admin".equals(username) && "123".equals(password)) {
+            StpUtil.login(username);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    @ApiOperation(value = "验证指定用户是否登录", notes = "传入一个用户名,判断其token是否合法")
+    @GetMapping("/isLogin")
+    public Boolean isLogin(String username) {
+        return StpUtil.isLogin(username);
+    }
+
 }

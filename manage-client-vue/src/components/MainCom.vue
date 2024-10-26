810cf13bca38732a91ec9dd95ade693b5b6ae5ce
江建清 <1810422805@qq.com>
2024-10-26 21:00:21 +0800

    fix:解决内容展示区高度被限制的问题

--- manage-client-vue/src/components/MainCom.vue
+++ manage-client-vue/src/components/MainCom.vue
@@ -2,7 +2,7 @@
  * @Author: 江建清 1810422805@qq.com
  * @Date: 2024-10-12 20:14:38
  * @LastEditors: 江建清 1810422805@qq.com
- * @LastEditTime: 2024-10-25 22:07:12
+ * @LastEditTime: 2024-10-26 20:54:53
  * @FilePath: \manage-client-vue\src\components\MainCom.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
--- manage-client-vue/src/components/NavigateCom.vue
+++ manage-client-vue/src/components/NavigateCom.vue
@@ -2,7 +2,7 @@
  * @Author: 江建清 1810422805@qq.com
  * @Date: 2024-10-25 22:04:51
  * @LastEditors: 江建清 1810422805@qq.com
- * @LastEditTime: 2024-10-25 22:39:22
+ * @LastEditTime: 2024-10-26 20:57:16
  * @FilePath: \manage-client-vue\src\components\NavigateCom.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -15,7 +15,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <el-container class="layout-container-demo" style="height: 500px">
+  <el-container class="layout-container-demo" style="height: 100vh">
     <el-aside width="200px">
       <el-scrollbar>
         <el-menu>

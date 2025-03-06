bd3caa7c84f9f422dbd8a0f47138d50433253be8
江建清 <1810422805@qq.com>
2025-03-06 13:58:11 +0800

    feat:Introduce custom log middleware

--- my-home-server-express-js/app.js
+++ my-home-server-express-js/app.js
@@ -3,9 +3,14 @@ const app = express(); // 创建一个 Express 应用实例
 const port = 3000; // 定义服务器运行的端口号
 // 引入自定义路由
 const myRouter = require('./src/routes/index.js');
+// 引入日志中间件
+const loggerMiddleware = require('./src/middlewares/loggerMiddleware.js');
 
+// 注册日志中间件(需要在路由的前面注册)
+app.use(loggerMiddleware);
 // 注册路由
 app.use('/', myRouter);
+
 
 // 启动服务器
 app.listen(port, () => {

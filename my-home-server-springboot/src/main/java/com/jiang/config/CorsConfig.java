5d73bf290fc2b47cf71d4d0b3a4d73f52ce69b61
江建清 <1810422805@qq.com>
2025-03-18 02:44:49 +0800

    feat:Dynamically set the IP addresses that are allowed to be accessed through variables

--- my-home-server-springboot/src/main/java/com/jiang/config/CorsConfig.java
+++ my-home-server-springboot/src/main/java/com/jiang/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.jiang.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    // 允许访问的IP地址
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // 添加映射路径
         registry.addMapping("/**")
-                .allowedOrigins("*") // 允许任何来源访问
+                .allowedOrigins(allowedOrigins.split(",")) // 允许指定来源的IP地址
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
                 .allowedHeaders("*") // 允许的请求头
                 .maxAge(168000); // 预检请求的缓存时间（以秒为单位）

62a88b0fa6b8e47c1408d7f4dd8f9022f4f8b43d
江建清 <1810422805@qq.com>
2025-04-12 02:00:51 +0800

    feat:Reorganize routing

--- my-home-server-express-js/app.js
+++ my-home-server-express-js/app.js
@@ -1,8 +1,11 @@
+/* 项目入口 */
+'use strict'
+
 const express = require('express') // 引入 Express 模块
 const app = express() // 创建一个 Express 应用实例
 const port = 3000 // 定义服务器运行的端口号
 // 引入自定义路由
-const myRouter = require('./src/routes/index.js')
+const router = require('./src/routes/index.js')
 // 引入日志中间件
 const loggerMiddleware = require('./src/middlewares/loggerMiddleware.js')
 
@@ -11,7 +14,7 @@ app.use(express.static('./src/public'))
 // 注册日志中间件(需要在路由的前面注册)
 app.use(loggerMiddleware)
 // 注册路由
-app.use('/', myRouter)
+app.use('/', router)
 
 // 启动服务器
 app.listen(port, () => {
--- my-home-server-express-js/src/routes/index.js
+++ my-home-server-express-js/src/routes/index.js
@@ -1,29 +1,16 @@
 /**
  * 创建时间: 2025-03-06
  * 作者: jjq
- * 描述: 路由文件
+ * 描述: 根路由文件
  */
+'use strict'
 
 const express = require('express')
-const myRouter = express.Router()
-// 测试prisma
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
+const router = express.Router()
 
-myRouter.get('/', (req, res) => {
-    res.send('自定义路由测试！')
-})
-
-// 测试路由：获取所有用户
-myRouter.get('/users', async (req, res) => {
-    try {
-        const users = await prisma.users.findMany()
-        res.json(users)
-    } catch (error) {
-        console.error(error)
-        res.status(500).send('Internal Server Error')
-    }
+router.get('/', (req, res) => {
+    res.send('hello express')
 })
 
 // 导出路由
-module.exports = myRouter
+module.exports = router

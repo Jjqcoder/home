2dc2a824223ada543a2656b789e122bc5f1d4fca
江建清 <1810422805@qq.com>
2025-04-11 23:52:25 +0800

    style:Set the code style with settings.json

--- my-home-server-express-js/src/routes/index.js
+++ my-home-server-express-js/src/routes/index.js
@@ -4,27 +4,26 @@
  * 描述: 路由文件
  */
 
-const express = require('express');
-const myRouter = express.Router();
+const express = require('express')
+const myRouter = express.Router()
 // 测试prisma
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
 
 myRouter.get('/', (req, res) => {
-  res.send('自定义路由测试！');
-});
-
+    res.send('自定义路由测试！')
+})
 
 // 测试路由：获取所有用户
 myRouter.get('/users', async (req, res) => {
-  try {
-    const users = await prisma.users.findMany();
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+    try {
+        const users = await prisma.users.findMany()
+        res.json(users)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Internal Server Error')
+    }
+})
 
 // 导出路由
-module.exports = myRouter;
+module.exports = myRouter

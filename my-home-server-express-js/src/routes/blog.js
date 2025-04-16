d15b3b05028072632454e6910e178472e4c71f26
江建清 <1810422805@qq.com>
2025-04-17 00:25:52 +0800

    chore:Refine annotations and introduce strict patterns

--- my-home-server-express-js/src/routes/blog.js
+++ my-home-server-express-js/src/routes/blog.js
@@ -1,3 +1,11 @@
+'use strict'
+
+/**
+ * 创建时间: 2025-04-17
+ * 作者: jjq
+ * 描述: 日志路由文件
+ */
+
 const express = require('express')
 const router = express.Router()
 
--- my-home-server-express-js/src/routes/index.js
+++ my-home-server-express-js/src/routes/index.js
@@ -1,9 +1,10 @@
+'use strict'
+
 /**
  * 创建时间: 2025-03-06
  * 作者: jjq
  * 描述: 根路由文件
  */
-'use strict'
 
 const express = require('express')
 const router = express.Router()

f46f0702425f45eb056c3f388d1ee2e4d097d444
江建清 <1810422805@qq.com>
2025-05-08 22:45:28 +0800

    feat:Deployed using the dockerfile successfully

--- microservices/db-service/app.js
+++ microservices/db-service/app.js
@@ -1,9 +1,1 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
-
 require('./lib/rabbitmq/rabbitmq.js')
-// 启动服务器
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
-})

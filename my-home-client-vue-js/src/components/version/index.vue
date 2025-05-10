0507ebb963cd822017c28e2b86d15fd99a6b2772
江建清 <1810422805@qq.com>
2025-05-10 23:11:35 +0800

    chore:Beautify the version page

--- my-home-client-vue-js/src/components/version/index.vue
+++ my-home-client-vue-js/src/components/version/index.vue
@@ -5,10 +5,10 @@
 </template>
 
 <script setup>
-import DataDisplay from '../../components/DataDisplay/index.vue'
 import {ref, watch} from 'vue'
-import {get} from './../../utils/api/index.js'
+import DataDisplay from '../../components/DataDisplay/index.vue'
 import {version as clientVersion} from './../../../package.json'
+import {get} from './../../utils/api/index.js'
 
 // 获取前端版本信息
 let serverVersion = ref(null)
@@ -28,17 +28,54 @@ getServerVersion()
 
 // 初始化需要渲染的内容
 let htmlContent = ref(`
-    <p>前端版本：${clientVersion}</p>
-    <p>后端版本：${serverVersion.value}</p>
+    <div class="version-info">
+        <div class="version-item">
+            <span class="label">前端版本：</span>
+            <span class="value">${clientVersion}</span>
+        </div>
+        <div class="version-item">
+            <span class="label">后端版本：</span>
+            <span class="value">${serverVersion.value}</span>
+        </div>
+    </div>
 `)
 
 // 使用watch 确保渲染的是最新的版本信息（避免网络延迟 导致渲染错误）
 watch(serverVersion, () => {
     htmlContent.value = `
-        <p>前端版本：${clientVersion}</p>
-        <p>后端版本：${serverVersion.value}</p>
+        <div class="version-info">
+            <div class="version-item">
+                <span class="label">前端版本：</span>
+                <span class="value">${clientVersion}</span>
+            </div>
+            <div class="version-item">
+                <span class="label">后端版本：</span>
+                <span class="value">${serverVersion.value}</span>
+            </div>
+        </div>
     `
 })
 </script>
 
-<style></style>
+<style>
+.version-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* height: 100vh; 使容器占满整个视口高度 */
+    font-size: 1.5em; /* 字体变大 */
+}
+
+.version-item {
+    margin: 10px 0; /* 上下外边距，使两个版本信息之间有间隔 */
+}
+
+.version-item .label {
+    font-weight: bold; /* 标签加粗 */
+}
+
+.version-item .value {
+    color: #555; /* 值的颜色稍浅 */
+}
+</style>

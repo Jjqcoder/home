29383450b56d7ecdf0d53ee75ab1d9bf2409a4fe
江建清 <1810422805@qq.com>
2025-05-14 17:03:36 +0800

    feat:git no longer tracks private files

---
+++ microservices/db-service/config-sample/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+    ip: 'xxx',
+    rabbitmq_user: 'xx',
+    rabbitmq_password: 'xx',
+    rabbitmq_port: xx
+}
--- microservices/db-service/config/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = {
-    ip: '120.27.159.194',
-    rabbitmq_user: 'myuser',
-    rabbitmq_password: 'mypassword',
-    rabbitmq_port: 5672
-}

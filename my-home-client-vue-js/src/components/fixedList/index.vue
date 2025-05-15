7147cd0fa43a9c3dbacc94fb742bf9ccebcea802
江建清 <1810422805@qq.com>
2025-05-16 01:31:49 +0800

    chore:Remove data of blog unnecessarily

--- my-home-client-vue-js/src/components/fixedList/index.vue
+++ my-home-client-vue-js/src/components/fixedList/index.vue
@@ -2,7 +2,8 @@
     <div class="blog-list-container" :style="lightEffect">
         <div class="blog-list-content">
             <el-table :data="pageSelectData" style="width: 100%" height="100%" class="blog-table">
-                <el-table-column fixed prop="ID" label="日志标识" width="" />
+                <!-- 注：不再显示日志标识。标识信息对用户是没用的，开发过程中需要的时候取用即可，不必显示 -->
+                <!-- <el-table-column fixed prop="ID" label="日志标识" width="" /> -->
                 <el-table-column prop="BLOG_TITLE" label="日志标题" width="" />
                 <el-table-column prop="BLOG_CREATE_TIME" label="创建时间" width="" />
                 <el-table-column prop="BLOG_UPDATE_TIME" label="更新时间" width="" />

8035bba14278ea777df9af35ab6bd57c99736868
江建清 <1810422805@qq.com>
2025-05-19 02:07:37 +0800

    chore:Optimize the code structure

--- microservices/db-service/middleware/index.js
+++ microservices/db-service/middleware/index.js
@@ -4,21 +4,17 @@
  * 创建时间: 2025-05-18
  * 作者: jjq
  * 描述: 中间件类
- * 约定：业务流程中，只会向请求方响应一次，一旦响应，后续中间件或者业务代码不再执行
+ * 约定：业务流程中，只会向请求方响应一次，一旦响应，后续中间件或者业务代码不再执行。保证整个业务流程会有返回值。
  */
+
+const R = require('../R.js')
 
 module.exports = class Middleware {
     // 前置中间件
-    static async preCheck(ctx, next) {
-        console.log('前置中间件执行了')
-        next()
+    static async securityMiddleware(req) {
+        console.log('我是安全前置中间件，入参为' + JSON.stringify(req))
+        if (1) {
+            return R.err(400, '请求不合法', null)
+        }
     }
-    // 后置中间件
-    static async postCheck(ctx, next) {
-        console.log('后置中间件执行了')
-        next()
-    }
-
-    // 中间件执行器
-    static middlewareExecutor = require('./middlewareExecutor.js')
 }
--- microservices/db-service/router/manage-server-express-js/index.js
+++ microservices/db-service/router/manage-server-express-js/index.js
@@ -9,22 +9,26 @@ const R = require('../../R.js')
 
 // 涉及数据库的操作均在此服务完成
 const router = {
+    /* 分页获取日志 */
     '/getBlogByPage': [
-        async req => {
-            console.log('前置中间件执行了,入参为' + JSON.stringify(req)) // 此中间件如果返回了数据，则后续的中间件不再执行。如果没有返回数据，则会继续执行后续的中间件
-        },
-        // async () => {
-        //     return R.err(400, '参数错误', null) // 直接返回数据，后续的中间件不再执行
-        // },
-        controller.getBlogByPage, // 返回数据
-        async req => {
-            console.log('后置中间件执行了,入参为' + JSON.stringify(req)) // 因为业务代码返回了数据，所以此中间件不会执行
-        }
-    ], // 分页获取日志
-    '/getSentenceRandomOne': controller.getSentenceRandomOne, // 随机获取一个英语句子
-    '/getAllVisit': controller.getAllVisit, // 获取所有访问记录
-    '/getVisitByLimit': controller.getVisitByLimit // 获取近期访问记录
-
+        require('../../middleware/index.js').securityMiddleware, // 安全前置中间件
+        controller.getBlogByPage
+    ],
+    // 随机获取一个英语句子
+    '/getSentenceRandomOne': [
+        require('../../middleware/index.js').securityMiddleware, // 安全前置中间件
+        controller.getSentenceRandomOne
+    ],
+    // 获取所有访问记录
+    '/getAllVisit': [
+        require('../../middleware/index.js').securityMiddleware, // 安全前置中间件
+        controller.getAllVisit
+    ],
+    // 获取近期访问记录
+    '/getVisitByLimit': [
+        require('../../middleware/index.js').securityMiddleware, // 安全前置中间件
+        controller.getVisitByLimit
+    ]
     // 可以继续添加更多方法
 }
 

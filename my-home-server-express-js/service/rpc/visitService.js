f23b87560d58e46ebe5875729cdc666c23008602
江建清 <1810422805@qq.com>
2025-05-25 21:35:32 +0800

    fea:Complete the adjustment of the visit interface

--- my-home-server-express-js/service/rpc/visitService.js
+++ my-home-server-express-js/service/rpc/visitService.js
@@ -11,7 +11,14 @@ const {prisma} = require('../../lib/index.js')
 module.exports = class VisitService {
     static async getAllVisit(req, res) {
         try {
-            return await prisma.vISIT_RECORDER.findMany()
+            let res = await RabbitMQRequester.sendRequest('/manage-server-express-js', {route: '/getAllVisit', data: {...req.query}})
+            res = JSON.parse(res)
+            if (res.code === 200) {
+                console.log('结果:', res.data)
+                return res.data
+            } else {
+                throw res.msg
+            }
         } catch (error) {
             throw error
         }
@@ -20,17 +27,14 @@ module.exports = class VisitService {
     // 获取指定数量的数据 默认为500条（前端不会发送limit数量）
     static async getVisitByLimit(req, res) {
         try {
-            // const limit = req.query.limit || 500 // 默认为500条
-            const limit = 1000 // 默认为500条
-
-            let res = await prisma.vISIT_RECORDER.findMany({
-                take: limit, // 限制返回的记录数
-                orderBy: {
-                    ID: 'desc' // 按id降序排序
-                }
-            })
-
-            return res.reverse() // 注：这里返回的数据是倒序的，所以需要反转一下
+            let res = await RabbitMQRequester.sendRequest('/manage-server-express-js', {route: '/getVisitByLimit', data: {...req.query}})
+            res = JSON.parse(res)
+            if (res.code === 200) {
+                console.log('结果:', res.data)
+                return res.data
+            } else {
+                throw res.msg
+            }
         } catch (error) {
             throw error
         }

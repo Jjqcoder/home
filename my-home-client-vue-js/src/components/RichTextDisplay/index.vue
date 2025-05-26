68bb9b0ba68e8226f724b37fd992410bfa101565
江建清 <1810422805@qq.com>
2025-05-26 21:46:00 +0800

    feat:Improve the display function of the log

--- my-home-client-vue-js/src/components/RichTextDisplay/index.vue
+++ my-home-client-vue-js/src/components/RichTextDisplay/index.vue
@@ -1,23 +1,24 @@
 <template>
-    <div>
-        <h1 style="text-align: center">富文本展示组件</h1>
-        <div v-html="editorContent"></div>
-    </div>
+    <div v-html="editorContent"></div>
 </template>
 
 <script setup>
 import {ref} from 'vue'
 
+const props = defineProps({
+    blog: {
+        type: String, // 或根据你的实际类型调整
+        required: true
+    }
+})
+console.log('子组件', props.blog)
+const blogData = JSON.parse(props.blog)
+
 // 模拟从数据库获取的富文本内容
 const editorContent = ref(`
-    <h2>标题</h2>
-    <p>这是一个段落文本，<strong>加粗</strong>的文字，<em>斜体</em>的文字。</p>
-    <ul>
-      <li>列表项1</li>
-      <li>列表项2</li>
-      <li>列表项3</li>
-    </ul>
-    <img src="https://picsum.photos/200" alt="示例图片">
+    <h2 style="text-align: center">${blogData.BLOG_TITLE}</h2>
+    <p>【创建时间: ${blogData.BLOG_CREATE_TIME}】</p><p>【更新时间: ${blogData.BLOG_UPDATE_TIME}】</p>
+    <div style="text-indent: 2em">${blogData.BLOG_CONTENT}</div>
   `)
 </script>
 
--- my-home-client-vue-js/src/components/blogDetail/index.vue
+++ my-home-client-vue-js/src/components/blogDetail/index.vue
@@ -1,9 +1,20 @@
 <template>
-    <RichTextDisplay />
+    <RichTextDisplay :blog="blog" />
 </template>
 
 <script setup>
 import RichTextDisplay from '../RichTextDisplay/index.vue'
+// 获取日志信息
+import {useRoute} from 'vue-router'
+
+const route = useRoute()
+
+// 获取动态参数 row
+const blog = route.params.row
+/*
+console.log(blog)
+{"ID":1,"BLOG_CREATE_TIME":"2025-03-09 14:45:17","BLOG_UPDATE_TIME":"2025-03-09 15:01:38","BLOG_TITLE":"xx","BLOG_CONTENT":"xx","IS_DELETE":"0"}
+*/
 </script>
 
 <style lang="scss" scoped></style>

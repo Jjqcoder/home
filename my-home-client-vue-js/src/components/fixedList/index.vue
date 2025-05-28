095d309e812b9a1fce0bf92baa3d56d572231ba4
江建清 <1810422805@qq.com>
2025-05-28 23:07:27 +0800

    chore:Adjust the component style

--- my-home-client-vue-js/src/components/fixedList/index.vue
+++ my-home-client-vue-js/src/components/fixedList/index.vue
@@ -5,10 +5,10 @@
             <el-table :data="pageSelectData" style="width: 100%" height="100%" class="blog-table">
                 <!-- 注：不再显示日志标识。标识信息对用户是没用的，开发过程中需要的时候取用即可，不必显示 -->
                 <!-- <el-table-column fixed prop="ID" label="日志标识" width="" /> -->
-                <el-table-column prop="BLOG_TITLE" label="日志标题" width="" />
+                <el-table-column prop="BLOG_TITLE" fixed="left" label="日志标题" width="" />
                 <el-table-column prop="BLOG_CREATE_TIME" label="创建时间" width="" />
                 <el-table-column prop="BLOG_UPDATE_TIME" label="更新时间" width="" />
-                <el-table-column fixed="right" label="操作" width="">
+                <el-table-column fixed="right" label="操作" width="100">
                     <template v-slot="scope">
                         <el-button @click="row_click(scope.row)" size="small" class="view-button"> 查看 </el-button>
                     </template>

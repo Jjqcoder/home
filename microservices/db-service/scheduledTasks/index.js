939cb28ee3f0498fcce4bcd8b7c90a20d3142206
江建清 <1810422805@qq.com>
2025-06-10 12:40:15 +0800

    feat(scheduledTasks):Introducing safe-timers

--- microservices/db-service/scheduledTasks/index.js
+++ microservices/db-service/scheduledTasks/index.js
@@ -4,22 +4,11 @@
  * 描述: 本文件由 PM2 单实例运行，专门管理定时任务，与多实例运行的 app.js 主应用分离，避免任务重复执行。
  */
 
-/* 定时任务定义开始 */
-let job1 = (() => {
-    // 立即执行函数的代码
-    return function() {
-      // 返回的函数逻辑
-      setInterval(() => {
-        console.log('每5秒执行一次')
-      }, 5000)
-    };
-  })();
-/* 定时任务定义结束 */
+// 文档：https://www.npmjs.com/package/safe-timers
+const setInterval = require('safe-timers').setInterval;
+ 
+setInterval(function (msg) {
+  console.log(msg);
+}, 1000, '每秒执行一次');
 
-/* ========================== */
-
-/* 启动定时任务开始 */
-job1()
-/* 启动定时任务结束 */
-
-module.exports = job1
+// 注：此文件内容由pm2调用 所以此处不需要导出内容

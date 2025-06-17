6780435d38dabc78875f7c1a195ec068700c4afd
江建清 <1810422805@qq.com>
2025-06-17 22:45:39 +0800

    feat:Introducing micro-pane

--- my-home-client-vue-js/src/main.js
+++ my-home-client-vue-js/src/main.js
@@ -11,6 +11,14 @@ import router from './router'; // 导入路由
 startInfo()
 /* 项目启动之后打印相关信息方便调试结束 */
 
+/* 弹窗库测试开始 */
+// 暂不启用
+// 用户点击屏幕之后 进行弹窗
+// document.addEventListener('click', (e) => {
+//     Pane.show('你点击了屏幕', 'success');
+// })
+/* 弹窗库测试结束 */
+
 /* 公告开始 */
 // 目前暂不启用
 const notification = new Notification({ content: '【测试】这是一个测试通知' });

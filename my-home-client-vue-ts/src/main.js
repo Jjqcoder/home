18d36c8d5dd5e2f86c392d46f9dc4fd595d59c8c
江建清 <1810422805@qq.com>
2025-06-18 15:10:13 +0800

    refactor(main.js):the page rendering logic was optimized to solve the white screen problem

--- my-home-client-vue-ts/src/main.js
+++ my-home-client-vue-ts/src/main.js
@@ -6,7 +6,6 @@ import {
     ClickEffects,
     enableCustomSelectionStyle,
     initEyeProtection,
-    Notification,
     setPointingCursor,
     showSplash,
     startInfo
@@ -35,33 +34,33 @@ setPointingCursor()
 
 async function initializeApp() {
     /* 显示入场动画开始 */
-    await showSplash('/img/windy.webp', 'Hello traveler!', 'rgba(218, 165, 32, 0.8)')
-    // await showSplash('/img/furina.jpg', 'Hello traveler!', 'rgba(47, 164, 231, 0.8)')
+    // await showSplash('/img/windy.webp', 'Hello traveler!', 'rgba(218, 165, 32, 0.8)')
+    await showSplash('/img/furina.jpg', 'Hello traveler!', 'rgba(47, 164, 231, 0.8)')
     /* 显示入场动画结束 */
-
-    /* 正常渲染页面开始 */
-    createApp(App).use(router).mount('#app')
-    /* 正常渲染页面结束 */
-
-    /* 护眼模式相关内容开始 */
-    initEyeProtection();
-    /* 护眼模式相关内容结束 */
-
-    /* 美化点击效果开始 */
-    new ClickEffects();
-    /* 美化点击效果结束 */
-
-    /* 文字选中样式开始 */
-    enableCustomSelectionStyle()
-    /* 文字选中样式结束 */
-
-    /* 公告开始 */
-    const notification = new Notification({ content: '【测试】这是一个测试通知' });
-    notification.show();
-    /* 公告结束 */
-
-    /*demo开始*/
-    /*demo结束*/
-
 }
 initializeApp()
+// ===========================================不影响后续的页面渲染，解决白屏问题开始===========================================
+/* 正常渲染页面开始 */
+createApp(App).use(router).mount('#app')
+/* 正常渲染页面结束 */
+
+/* 护眼模式相关内容开始 */
+initEyeProtection();
+/* 护眼模式相关内容结束 */
+
+/* 美化点击效果开始 */
+new ClickEffects();
+/* 美化点击效果结束 */
+
+/* 文字选中样式开始 */
+enableCustomSelectionStyle()
+/* 文字选中样式结束 */
+
+/* 公告开始 */
+// 注意 公告不要在此处进行发布 因为用户如果在开场图片出停留迟迟不进入主页面，页面会进行卡顿
+// 而如果通过阻塞主页面渲染，虽然能让用户一进入页面就看到公告，但是会有白屏现象、
+// 后续计划将公告单独封装成一个组件！
+// const notification = new Notification({ content: '【测试】这是一个测试通知' });
+// notification.show();
+/* 公告结束 */
+// ===========================================不影响后续的页面渲染，解决白屏问题开始===========================================

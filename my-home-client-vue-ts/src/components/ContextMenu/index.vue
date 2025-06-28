fc04a498497b0c95d83cf0ae5d4365a07347ab43
江建清 <1810422805@qq.com>
2025-06-29 04:25:21 +0800

    feat(ContextMenu, FloatToolBox):initializing new components

---
+++ my-home-client-vue-ts/src/components/ContextMenu/index.vue
@@ -0,0 +1,13 @@
+<template>
+  右键点击展示内容的组件
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-home-client-vue-ts/src/components/FloatToolbox/index.vue
@@ -0,0 +1,13 @@
+<template>
+  悬浮于网页右下角的工具组件
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

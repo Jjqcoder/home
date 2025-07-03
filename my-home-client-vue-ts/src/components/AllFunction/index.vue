3df4b72f8b95e80f8e55ab369e9d4df1033e32a0
江建清 <1810422805@qq.com>
2025-07-03 22:46:36 +0800

    refactor(AllFunction):switch AllFunctions component language to english only

--- my-home-client-vue-ts/src/components/AllFunction/index.vue
+++ my-home-client-vue-ts/src/components/AllFunction/index.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-wrap items-center my-font-style">
         <el-dropdown>
             <el-button type="primary">
-                全部功能<el-icon class="el-icon--right">
+                All functions<el-icon class="el-icon--right">
                     <Watermelon />
                 </el-icon>
             </el-button>
@@ -13,39 +13,39 @@
                     </router-link> -->
 
                     <router-link to="/">
-                        <el-dropdown-item>🏠回首页</el-dropdown-item>
+                        <el-dropdown-item>🏠Back to Home</el-dropdown-item>
                     </router-link>
 
                     <router-link to="/blog">
-                        <el-dropdown-item>✍️看日志</el-dropdown-item>
+                        <el-dropdown-item>✍️View Blogs</el-dropdown-item>
                     </router-link>
                     <router-link to="/english">
-                        <el-dropdown-item>🔠学英语</el-dropdown-item>
+                        <el-dropdown-item>🔠Learn English</el-dropdown-item>
                     </router-link>
                     <router-link to="/weather">
-                        <el-dropdown-item>🌤️查天气</el-dropdown-item>
+                        <el-dropdown-item>🌤️Check the Weather</el-dropdown-item>
                     </router-link>
                     <router-link to="/visitorStatistics">
-                        <el-dropdown-item>🔢看数据</el-dropdown-item>
+                        <el-dropdown-item>🔢View Data</el-dropdown-item>
                     </router-link>
-                    <!-- 跳转到本站的管理后台 后续将开放游客模式让游客体验后台的功能（功能会限制） -->
+                    <!-- Go to Admin Panel (Visitor Mode coming soon – experience limited backend functionality.) -->
                     <!-- <a href="http://wbeishangw.top:81/" target="_blank">
-                        <el-dropdown-item>🎛️管后台</el-dropdown-item>
+                        <el-dropdown-item>🎛️Admin Panel</el-dropdown-item>
                     </a> -->
                     <router-link to="/version">
-                        <el-dropdown-item>👓看版本</el-dropdown-item>
+                        <el-dropdown-item>👓View Version</el-dropdown-item>
                     </router-link>
                     <!-- <router-link to="/login">
-                        <el-dropdown-item>📲去登录</el-dropdown-item>
+                        <el-dropdown-item>📲Go to Login</el-dropdown-item>
                     </router-link> -->
                     <!-- <router-link to="/game">
-                        <el-dropdown-item>👻玩游戏</el-dropdown-item>
+                        <el-dropdown-item>👻Play Games</el-dropdown-item>
                     </router-link> -->
                     <!-- <router-link to="/aboutWebsite">
-                        <el-dropdown-item>🧑‍💻关于本站</el-dropdown-item>
+                        <el-dropdown-item>🧑‍💻About This Site</el-dropdown-item>
                     </router-link> -->
                     <router-link to="/about">
-                        <el-dropdown-item>🤟关于我</el-dropdown-item>
+                        <el-dropdown-item>🤟About Me</el-dropdown-item>
                     </router-link>
                 </el-dropdown-menu>
             </template>

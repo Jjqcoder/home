9f9b2b6bfd1a988a5053f9f2c6ebcd9231b6cf0b
江建清 <1810422805@qq.com>
2025-07-12 22:53:18 +0800

    feat(element-plus):introducing element-plus

--- manage-client-vue/src/main.ts
+++ manage-client-vue/src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
+
+/* introducing element-plus start */ 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+/* introducing element-plus end */ 
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+/* use element-plus start*/ 
+app.use(ElementPlus)
+/* use element-plus end*/ 
+app.mount('#app')
